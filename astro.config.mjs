ed2be99c130e36ad135bb59700c3d869176c2d52
Tu Nombre <angelpaqui36@gmail.com>
2026-07-30 21:54:19 -0400

    Enable Server-Side Rendering (SSR) for dynamic pages to instantly recognize newly created products and categories

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: 'https://eltrigalbakery.com',
   i18n: {
     defaultLocale: 'es',
