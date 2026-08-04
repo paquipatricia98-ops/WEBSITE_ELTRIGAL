54a958454797b49e592c7a25d52aae1be62a1342
Tu Nombre <angelpaqui36@gmail.com>
2026-08-03 22:06:56 -0400

    Add new files and .gitignore

---
+++ test_api.ts
@@ -0,0 +1,7 @@
+import { getProducts } from './src/services/api/publicApi';
+async function run() {
+  const { products } = await getProducts({ locale: 'es', limit: 100 });
+  console.log("Total length:", products.length);
+  console.log("Slugs:", products.map(p => p.slug?.es));
+}
+run();
