3543816e92a1c9055663cbf7ae1da9124bec97a4
Tu Nombre <angelpaqui36@gmail.com>
2026-08-06 20:29:19 -0400

    fix(catalog): resolve limit=200 api error and fix category links on homepage

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_BjQbrQcO.mjs';
-import { manifest } from './manifest_CfHmlYjU.mjs';
+import { manifest } from './manifest_D18uAFsF.mjs';
 
 const serverIslandMap = new Map();;
 
